c58fdc95dc39bf9906dd41471d73005cbda7a842
Ashlin Harris <ashlin_harris@brown.edu>
2022-03-30 14:48:21 -0400

    Remove deprecated code

--- src/MLDemo.jl
+++ src/MLDemo.jl
@@ -5,7 +5,6 @@ export add_target_column!, get_data, dataframe_subset, boolean_unstack, run_deci
 using ConfParser # Parse, modify, write to configuration files
 using DataFrames
 using MLJ
-#load_path("DecisionTreeClassifier", pkg="DecisionTree")
 using MLJDecisionTreeInterface
 
 using CSV: File
@@ -39,7 +38,6 @@ end
 Return the contents of a CSV file as a DataFrame
 """
 function get_data(file_name::String)::AbstractDataFrame
-
 	conf = ConfParse("./config.ini")
 	parse_conf!(conf)
 	path = retrieve(conf, "local", "path")
@@ -71,43 +69,10 @@ Unstack a DataFrame df by row and column keys x and y
 Isn't there a one-liner for this?
 """
 function boolean_unstack(df::AbstractDataFrame, x::Symbol, y::Symbol)::AbstractDataFrame
-
-	#=
-	###OLD METHOD###
-	rows = df[!,x] |> sort |> unique
-	cols = df[!,y] |> sort |> unique
-
-	r_dict = Dict()
-	for k in 1:length(rows)
-		r_dict[rows[k]] = k
-	end
-	c_dict = Dict()
-	for k in 1:length(cols)
-		c_dict[cols[k]] = k
-	end
-
-	A = zeros(length(rows), length(cols))
-	for q in eachrow(df)
-		i = r_dict[q[x]]
-		j = c_dict[q[y]]
-		A[i, j] = true
-	end
-
-	A = DataFrame([Vector{Bool}(undef, length(rows)) for _ in eachcol(A)], :auto)
-	rename!(A, cols)
-	insertcols!(A, 1, x => rows, makeunique = true)
-	=#
-
-	###NEW METHOD###
 	B = unstack(combine(groupby(df, [x,y]), nrow => :count), x, y, :count, fill=0)
 	for q in names(select(B, Not(:PATIENT)))
 		B[!,q] = B[!,q] .!= 0
 	end
-	
-	###EXPERIMENTAL###
-	#B = DataFrame(colwise(col -> recode(col, missing=>0), B), names(B)
-	#B = combine(groupby(df, [x,y]), nrow => :count)
-
 	return B
 end
 
@@ -117,15 +82,12 @@ end
 Decision tree classifier on a DataFrame over a given output
 """
 function run_decision_tree(df::AbstractDataFrame, output::Symbol)::Tuple{Float64, Float64}
-
 	y = df[:, output]
 	X = select(df, Not([:PATIENT, output]))
 	
 	#TODO: make sure this is deterministic
 	RNG_VALUE = 2022
 	train, test = partition(eachindex(y), 0.8, shuffle = true, rng = RNG_VALUE)
-	#display(models(matching(X, y)))
-	#println()
 
 	Tree = @load DecisionTreeClassifier pkg=DecisionTree verbosity=0
 	tree_model = Tree(max_depth = 3)
@@ -146,13 +108,6 @@ end
 Find top n values by occurence
 """
 function top_n_values(df::DataFrame, col::Symbol, n::Int)::DataFrame
-	#name = @nameofvariable(df)
-	#println("Top $n values for $col in $name:")
-	#=
-	x = first(sort(countmap(df[:, col]; alg = :dict), byvalue = true, rev = true), n)
-	show(IOContext(stdout, :limit => false), "text/plain", x)
-	println()
-	=#
 	return first(sort(combine(nrow, groupby(df, col)), "nrow", rev=true), n)
 end
 
