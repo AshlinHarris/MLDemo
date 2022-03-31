bce656fe6a86ffcc7b5f5ec3feed383c350b86e1
Ashlin Harris <ashlin_harris@brown.edu>
2022-03-31 16:20:38 -0400

    Consistent RNG from main()

--- main.jl
+++ main.jl
@@ -9,7 +9,7 @@ using DataFrames
 #using LinearAlgebra
 using MLJ
 #using Profile
-#using Random
+using Random
 using Revise
 #using ScientificTypesBase
 #using SparseArrays
@@ -27,30 +27,33 @@ Machine Learning in Julia with Synthetic Data
 """
 function main()
 
+	SEED_VALUE = 2022
+	Random.seed!(SEED_VALUE)
+
 	# Read in DataFrames from files
 	conditions_df = get_data("conditions.csv")
-	allergy_df = get_data("allergies.csv")
+	allergies_df = get_data("allergies.csv")
 
 	# Summarize DataFrames
 	top_n_values(conditions_df, :DESCRIPTION, 12) |> println
-	top_n_values(allergy_df, :DESCRIPTION, 12) |> println
+	top_n_values(allergies_df, :DESCRIPTION, 12) |> println
 
 	# Filter DataFrames
 	miscarriage_only = dataframe_subset(conditions_df, "Miscarriage in first trimester")
-	with_allergies = dataframe_subset(allergy_df, miscarriage_only)
+	with_allergies = dataframe_subset(allergies_df, miscarriage_only)
 
 	# Generate composite DataFrame
-	composite_df = boolean_unstack(allergy_df, :PATIENT, :DESCRIPTION)
+	composite_df = boolean_unstack(allergies_df, :PATIENT, :DESCRIPTION)
 	add_target_column!(composite_df, :MISCARRIAGE, miscarriage_only)
 
 	# Machine learning
-	RNG_VALUE = 2022
+	RNG_VALUE = abs(rand(Int))
 	acc, f1_score= run_decision_tree(composite_df, :MISCARRIAGE, RNG_VALUE)
 	
 	# Results
 	println()
-	println(acc)
-	println(f1_score)
+	println("Accuracy: ", round(acc, digits=4))
+	println("F1 Score: ", round(f1_score, digits=4))
 
 	return nothing
 end
