9bd872f82ff25e581482e79fd46be3f4c1bb4691
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-07 13:36:38 -0400

    Generalize variable names for any topic

--- main.jl
+++ main.jl
@@ -23,13 +23,14 @@ function main()
 
 	# Read in DataFrames from files
 	conditions_df = get_data("conditions.csv")
-	allergies_df = get_data("allergies.csv")
+	topic_2_df = get_data("allergies.csv")
 	demographics_df = get_data("patients.csv")
 
 	# Filter DataFrames
-	topic = "Miscarriage in first trimester"
-	miscarriage_only = dataframe_subset(conditions_df, topic)
-	with_allergies = dataframe_subset(allergies_df, miscarriage_only)
+	topic_1 = "Hypertension"
+	#topic_1 = "Miscarriage in first trimester"
+	topic_1_only = dataframe_subset(conditions_df, topic_1)
+	with_topic_2 = dataframe_subset(topic_2_df, topic_1_only)
 
 	### ALLERGY STUDY ###
 
@@ -38,16 +39,16 @@ function main()
 	println()
 
 	# Summarize DataFrames
-	for df in [conditions_df, allergies_df]
+	for df in [conditions_df, topic_2_df]
 		println(top_n_values(df, :DESCRIPTION, 12))
 		println()
 	end
 
 	# Generate composite DataFrame
-	composite_df = boolean_unstack(allergies_df, :PATIENT, :DESCRIPTION)
-	add_target_column!(composite_df, :MISCARRIAGE, miscarriage_only)
+	composite_df = boolean_unstack(topic_2_df, :PATIENT, :DESCRIPTION)
+	add_target_column!(composite_df, :MISCARRIAGE, topic_1_only)
 
-	if nrow(with_allergies) != 0
+	if nrow(with_topic_2) != 0
 		# Machine learning
 		RNG_VALUE = abs(rand(Int))
 		acc, f1_score= run_decision_tree(composite_df, :MISCARRIAGE, RNG_VALUE)
@@ -61,37 +62,37 @@ function main()
 	### DEMOGRAPHICS ###
 
 	# Study feasibility
-	#selected = number_with(:DESCRIPTION, topic, conditions_df)
-	selected = nrow(miscarriage_only)
+	#selected = number_with(:DESCRIPTION, topic_1, conditions_df)
+	selected = nrow(topic_1_only)
 	total = nrow(conditions_df)
 
 	println()
-	@printf("%s: %s\n", "Study feasibility", topic)
+	@printf("%s: %s\n", "Study feasibility", topic_1)
 	@printf("%30s: %7d\n", "Total number of entries", total)
 	@printf("    %26s: %7d (%6.2f%%)\n", "Selected entries", selected, 100 * (selected / total))
-	#@printf("%30s: %7d\n", "Total number of entries", nrow(miscarriage_only))
+	#@printf("%30s: %7d\n", "Total number of entries", nrow(topic_1_only))
 	println()
 	println()
 
 	# From the demographics DataFrame, take only PATIENTS with "Miscarriage in first trimester"
 	#TODO: dataframe_subset() should be generalized to handle this
-	miscarriage_demographics = filter(:Id => x -> x in miscarriage_only.PATIENT, demographics_df)
-	#display(miscarriage_demographics)
-	#display(names(miscarriage_demographics))
+	topic_1_demographics = filter(:Id => x -> x in topic_1_only.PATIENT, demographics_df)
+	#display(topic_1_demographics)
+	#display(names(topic_1_demographics))
 
 	println()
 	println("Comparison of Demographic information")
 	println()
 	plots=[]
 	FACTORS = [:RACE, :ETHNICITY, :GENDER]
-	DATAFRAMES = [demographics_df, miscarriage_demographics]
+	DATAFRAMES = [demographics_df, topic_1_demographics]
 	for factor in FACTORS
 		for df in DATAFRAMES
 			x = top_n_values(df, factor, 12)
 			y = pie(x[!,factor], x.nrow) # Cannot handle missing values
 			push!(plots, y)
 		end
-		a = top_n_values(miscarriage_demographics, factor, 12)
+		a = top_n_values(topic_1_demographics, factor, 12)
 		rename!(a, Dict(:nrow => :Miscarriage))
 		b = top_n_values(demographics_df, factor, 12)
 		rename!(b, Dict(:nrow => :All))
